41a2d6f1a256f7d87fc3b8fe77ae0b6da9667bbf
wkru <48606886+wkru@users.noreply.github.com>
2019-05-23 04:14:16 +0200

    Updated the client to use the OpenShift platform

--- client/src/main/java/proz/webcalc/client/Model.java
+++ client/src/main/java/proz/webcalc/client/Model.java
@@ -13,6 +13,7 @@ import com.google.gson.Gson;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import jdk.jshell.JShell;
 
 class Expression {
 	private String expression;
@@ -25,43 +26,46 @@ class Expression {
 class Result {
 	private String result;
 	private boolean error;
+
 	public String getResult() {
 		return result;
 	}
+
 	public boolean isError() {
 		return error;
-	}	
+	}
 }
 
 public class Model {
-	public String calculate (String expression) {
+	public String calculate(String expression) {
 		Gson gson = new Gson();
 		Expression exp = new Expression();
 		Result result;
 		exp.setExpression(expression);
 		String json = gson.toJson(exp);
-		
-		//URI uri = URI.create("http://docker-proz123.apps.us-west-1.online-starter.openshift.com/");
-		URI uri = URI.create("http://localhost:9999/");
+
+		URI uri = URI.create("http://docker-proz123.apps.us-west-1.online-starter.openshift.com/"); //link valid until July 15, 2019
 		Client client = ClientBuilder.newClient();
-		
+
 		WebTarget webTarget = client.target(uri);
-		webTarget = webTarget.path("webcalc").path("calculate");
+		webTarget = webTarget.path("rest").path("calculate");
 
 		try {
-			Response response = webTarget.request(MediaType.APPLICATION_JSON).post(Entity.entity(json, MediaType.APPLICATION_JSON), Response.class);
-			
-			if(response.getStatus() != 200) {
+			Response response = webTarget.request(MediaType.APPLICATION_JSON)
+					.post(Entity.entity(json, MediaType.APPLICATION_JSON), Response.class);
+
+			if (response.getStatus() != 200) {
 				Alert alert = new Alert(AlertType.ERROR);
 				alert.setTitle("Błąd działania programu");
 				alert.setHeaderText("Błąd komunikacji z serwerem");
-				alert.setContentText("Sprawdź swoje połączenie z Internetem.\nWyczyść pamięć kalkulatora przyciskiem AC.");
+				alert.setContentText(
+						"Sprawdź swoje połączenie z Internetem.\nWyczyść pamięć kalkulatora przyciskiem AC.");
 				alert.showAndWait();
 				return "Błąd.";
 			}
-			
+
 			result = gson.fromJson(response.readEntity(String.class), Result.class);
-			
+
 		} catch (Exception e) {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setTitle("Błąd działania programu");
@@ -70,8 +74,8 @@ public class Model {
 			alert.showAndWait();
 			return "Błąd.";
 		}
-		
-		if(result.isError()) {
+
+		if (result.isError()) {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setTitle("Błąd działania programu");
 			alert.setHeaderText("W trakcie obliczeń napotkano błąd");
